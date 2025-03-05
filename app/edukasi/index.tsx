13d5e480aa3826b9f3e1eb434db24031739dd084
Nanda Surya <nandasurya@Mac-mini.local>
2025-03-05 12:08:41 +0800

    update video

--- app/edukasi/index.tsx
+++ app/edukasi/index.tsx
@@ -1,13 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
 import React from 'react'
-import BottomNavbar from '@/components/BottomNavbar'
+import YoutubeIframe from 'react-native-youtube-iframe';
+
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
-            <BottomNavbar />
-    </View>
+    <SafeAreaView className="flex-1 bg-gray-200">
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 16 }}>
+        <View className="flex-row justify-between items-center px-4 py-8 bg-white">
+          <View className="flex-1 ml-4">
+            <Text className="text-xl font-bold">Edukasi & Tips Daur Ulang</Text>
+          </View>
+        </View>
+        <View className="px-4 mt-4">
+          <View className="bg-white rounded-xl shadow-md p-4 w-full">
+            <Image
+              source={require("../../assets/images/edukasi.png")}
+              className="w-full h-44 rounded-xl mb-5"
+              resizeMode="cover"
+            />
+            <View>
+              <Text className="bg-gradientEnd py-2 px-2 w-1/3 text-white rounded-full text-center text-lg mb-4">Edukasi</Text>
+              <Text className="text-xl font-medium mb-4">Dari Sampah ke Barang Bernilai - Proses Daur Ulang Kertas, Plastik, dan Logam</Text>
+              <Text className="text-lg font-normal">4 hari yang lalu</Text>
+            </View>
+          </View>
+
+          <View className="bg-white rounded-xl shadow-md p-4 w-full mt-10">
+            <YoutubeIframe
+              height={200} 
+              play={true} 
+              videoId={"Dn7AROdyVKo"} 
+              onChangeState={(event) => console.log(event)} // Event listener untuk status video
+            />
+            <View>
+              <Text className="bg-gradientEnd py-2 px-2 w-1/3 text-white rounded-full text-center text-lg mb-4">Edukasi</Text>
+              <Text className="text-xl font-medium mb-4">Cara membuat lubang biopori</Text>
+              <Text className="text-lg font-normal">1 hari yang lalu</Text>
+            </View>
+          </View>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
